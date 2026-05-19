449b69956611f87717158f93ee739e1f7aad63aa
jasser.hasni <jasser.hasni@wecraft.tn>
2026-05-19 20:33:04 +0100

    Add curl retries and exit on error

--- Hadoop-deploy/deploy.sh
+++ Hadoop-deploy/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Setup local bin directory for binaries
 mkdir -p bin
@@ -7,7 +8,7 @@ export PATH=$PWD/bin:$PATH
 # Install helm if not available
 if ! command -v helm &> /dev/null; then
   echo "Installing helm..."
-  curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+  curl --retry 3 --retry-connrefused -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
   chmod 700 get_helm.sh
   HELM_INSTALL_DIR=$PWD/bin ./get_helm.sh --no-sudo
 fi
@@ -15,7 +16,8 @@ fi
 # Install kubectl if not available
 if ! command -v kubectl &> /dev/null; then
   echo "Installing kubectl..."
-  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+  KUBECTL_VERSION=$(curl --retry 3 -L -s https://dl.k8s.io/release/stable.txt)
+  curl --retry 3 --retry-connrefused -fLO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
   chmod +x kubectl
   mv kubectl bin/
 fi
